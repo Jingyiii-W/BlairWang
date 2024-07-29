5478e7f5d965cd50c2bce810be7d163283cfee20
Jingyiii-W <18jw111@gmail.com>
2024-07-29 07:46:42 -0400

    first verson

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+function showIntroductionPopup() {
+    alert("Do you know🧐"); // 弹窗提示
+    alert("Oranges Are Not the Only Fruit😉");
+    alert("But you are🥰"); 
+}
+
+function showIntroductionPopup1() {
+    alert("Trick or Treat!🎃");
+}
+
+function showIntroductionPopup2() {
+    alert("Trick or Treat!🎃");
+}
